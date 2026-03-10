6655a576dac40c897f899bce6d5c0ba214bcdf97
Pedro Luiz <0001156035@senaimgaluno.com.br>
2026-03-10 17:56:30 -0300

    Outro parte do exercicio feito

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-let elementoImagem = document.getElementById("cesar");
+function exibirMensagem(mensagem) {
+    alert("Você acessou a seção " + mensagem + ".");
+}
+
+const botao = document.getElementById("mostrarInformacoes");
+const conteudo = document.getElementById("conteudo");
+
+botao.addEventListener("click", () => {
+    conteudo.classList.toggle("escondido");
+
+    if (conteudo.classList.contains("escondido")) {
+        botao.textContent = "Mostrar mais informações";
+    } else {
+        botao.textContent = "Esconder as informações";
+    }
+});
+
+const elementoImagem = document.getElementById("cesar");
 const imagemOriginal = "imagens/Marmelo.png";
 const imagemDiferente = "imagens/Cesar.png";
 
